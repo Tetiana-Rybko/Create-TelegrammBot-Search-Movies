97736bb9fd9dbca6b48c3293f6bd3ccf354c3f44
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-02-27 23:36:39 +0300

    Add file connection

---
+++ con_db.py
@@ -0,0 +1,24 @@
+import pymysql.cursors
+
+
+class DatabaseManager:
+    def __init__(self,host,user,password,database):
+        try:
+           self.connection = pymysql.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=database,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+           self.cursor = self.connection.cursor()
+           print("Подключение к базе ОК")
+        except pymysql.MySQLError as e:
+            print("Ошибка подключения",e)
+            raise
+    def execute_query(self, query, params=None):
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchall()
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
